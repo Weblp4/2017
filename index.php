30fae39e08fd4bd802c883130bb0084b34118442
Alumno <Alumno@192.168.35.112>
2017-01-23 17:37:47 -0500

    pruebas 2

--- index.php
+++ index.php
@@ -10,8 +10,7 @@ and open the template in the editor.
         <title></title>
     </head>
     <body>
-        
-        hola
+          hola bienvenidos
         <?php
         // put your code here
         ?>
